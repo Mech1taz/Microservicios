27271e62335d64f83663649e340a4924d79fbe17
MechitaZ <ma.astudillom@duocuc.cl>
2025-05-16 11:20:44 -0400

    add modelo Alerta

---
+++ monitor/src/main/java/com/mechitaz/monitor/model/Alerta.java
@@ -0,0 +1,30 @@
+package com.mechitaz.monitor.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Alerta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String tipo;
+    private String mensaje;
+    private String severidad;
+
+
+public  boolean esCritico(){
+    return "ALTA".equalsIgnoreCase(severidad);
+}
+public void marcarComoLeido(){
+    mensaje += "(leido)";
+}
+}
